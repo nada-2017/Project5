3b1f938fd2ccc4fe9e9291475e56166870b03a6b
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-05-31 11:25:47 +0300

    Update Appointment.java

--- Salon1/src/main/java/com/example/salon1/Model/Appointment.java
+++ Salon1/src/main/java/com/example/salon1/Model/Appointment.java
@@ -33,10 +33,9 @@ public class Appointment {
     private Customer customer;
 
 
-    @ManyToOne
-    @JoinColumn(name = "staff_id" , referencedColumnName = "id")
+    @ManyToMany
     @JsonIgnore
-    private Staff staff;
+    private Set<Staff> staffSet;
 
     @ManyToMany
     @JsonIgnore
