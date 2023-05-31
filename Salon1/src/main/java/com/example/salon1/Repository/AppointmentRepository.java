3cc6e3d900a538ade08dec6135ddf2ff389765e6
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-05-31 13:06:44 +0300

    Update AppointmentRepository.java

--- Salon1/src/main/java/com/example/salon1/Repository/AppointmentRepository.java
+++ Salon1/src/main/java/com/example/salon1/Repository/AppointmentRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment,Integer> {
     Appointment findAppointmentById(Integer id);
+    
+    @Query("select a from Appointment a where a.day=?1")
+    List<Appointment> getAppointmentsByDay(Integer day);
 }
